1b9b95c780b35d3ee7768aa7719f5d9f6294cbca
diegopacheco <diego.pacheco.it@gmail.com>
2016-09-03 19:38:01 -0700

    clean up actor code

--- Chap8/scala-akka-actors/src/main/scala/actors/ActorFF.scala
+++ Chap8/scala-akka-actors/src/main/scala/actors/ActorFF.scala
@@ -18,12 +18,6 @@ object FireAndForgetActorMainApp extends App{
     val system = ActorSystem("SimpleActorSystem")
     val actor  = system.actorOf(Props[PrinterActor])
     
-    import scala.concurrent.duration._
-    import akka.util.Timeout
-    import akka.pattern.ask
-    import scala.concurrent.Await
-    implicit val timeout = Timeout(20 seconds) 
-    
     val voidReturn = actor ! Message
     println("Actor says: " + voidReturn )
     
